8e27129d1a31b2b41bd33b0a315ff65b2e85629e
Raimundas Banevicius <raima156@yahoo.com>
2020-05-25 13:13:40 +0300

    Student subject administration controler/views added.

---
+++ UniversityMvcProject/UniversityMvcProject/Controllers/StudentSubjectAdminController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UniversityMvcProject.Data;
+using UniversityMvcProject.Models;
+
+namespace UniversityMvcProject.Controllers
+{
+    public class StudentSubjectAdminController : Controller
+    {
+        private readonly UniversityMvcProjectContext _context;
+
+        public StudentSubjectAdminController(UniversityMvcProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StudentSubjectAdmin
+        public async Task<IActionResult> Index()
+        {
+            var universityMvcProjectContext = _context.StudentSubjects.Include(s => s.Student).Include(s => s.Subject);
+            return View(await universityMvcProjectContext.ToListAsync());
+        }
+
+        // GET: StudentSubjectAdmin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentSubject = await _context.StudentSubjects
+                .Include(s => s.Student)
+                .Include(s => s.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentSubject == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentSubject);
+        }
+
+        // GET: StudentSubjectAdmin/Create
+        public IActionResult Create()
+        {
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Name");
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name");
+            return View();
+        }
+
+        // POST: StudentSubjectAdmin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StudentId,SubjectId,Score")] StudentSubject studentSubject)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(studentSubject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Name", studentSubject.StudentId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", studentSubject.SubjectId);
+
+
+
+            return View(studentSubject);
+        }
+
+        // GET: StudentSubjectAdmin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentSubject = await _context.StudentSubjects.FindAsync(id);
+            if (studentSubject == null)
+            {
+                return NotFound();
+            }
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Name", studentSubject.StudentId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", studentSubject.SubjectId);
+            return View(studentSubject);
+        }
+
+        // POST: StudentSubjectAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StudentId,SubjectId,Score")] StudentSubject studentSubject)
+        {
+            if (id != studentSubject.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(studentSubject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentSubjectExists(studentSubject.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Name", studentSubject.StudentId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", studentSubject.SubjectId);
+            return View(studentSubject);
+        }
+
+        // GET: StudentSubjectAdmin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentSubject = await _context.StudentSubjects
+                .Include(s => s.Student)
+                .Include(s => s.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (studentSubject == null)
+            {
+                return NotFound();
+            }
+
+            return View(studentSubject);
+        }
+
+        // POST: StudentSubjectAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var studentSubject = await _context.StudentSubjects.FindAsync(id);
+            _context.StudentSubjects.Remove(studentSubject);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentSubjectExists(int id)
+        {
+            return _context.StudentSubjects.Any(e => e.Id == id);
+        }
+    }
+}
