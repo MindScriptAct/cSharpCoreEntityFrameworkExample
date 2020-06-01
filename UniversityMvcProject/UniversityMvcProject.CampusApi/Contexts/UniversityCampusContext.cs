00f0467191beb36334209e01423f81092386bbcf
Raimundas Banevicius <raima156@yahoo.com>
2020-06-01 15:04:34 +0300

    Context created. Test student controller added.

---
+++ UniversityMvcProject/UniversityMvcProject.CampusApi/Contexts/UniversityCampusContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UniversityMvcProject.Data.Models;
+
+namespace UniversityMvcProject.CampusApi.Contexts
+{
+    public class UniversityCampusContext : DbContext
+    {
+
+        public UniversityCampusContext(DbContextOptions<UniversityCampusContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ UniversityMvcProject/UniversityMvcProject.CampusApi/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniversityMvcProject.CampusApi.Contexts;
+using UniversityMvcProject.Data.Models;
+
+namespace UniversityMvcProject.CampusApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly UniversityCampusContext context;
+
+        public StudentController(UniversityCampusContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("getAllStudents")]
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return context.Students.ToList();
+        }
+
+
+    }
+}
