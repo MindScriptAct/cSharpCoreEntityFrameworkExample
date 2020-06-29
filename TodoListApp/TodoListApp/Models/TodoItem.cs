09c26e72536d020ff638c9d6eff042e4e6a99f91
Raimundas Banevicius <raima156@yahoo.com>
2020-06-29 13:13:03 +0300

    Todo items adedd to EF.

---
+++ TodoListApp/TodoListApp/Models/TodoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DeadLineDate { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Preority { get; set; } = 3;
+
+        [Required]
+        public TodoStatus Status { get; set; } = TodoStatus.Backlog;
+
+
+
+    }
+}
